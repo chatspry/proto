3d5637504e551db2bfabb9e481790a78798fca1b
Philip Vieira <philip@vallin.se>
2015-03-26 00:36:16 +0000

    Clean up context spec

--- spec/protocore/context_spec.rb
+++ spec/protocore/context_spec.rb
@@ -2,27 +2,26 @@ RSpec.describe Protocore::Context do
 
   include FakeFS::SpecHelpers
 
-  let(:work_dir) { "/tmp/protocore" }
-  subject(:context) { described_class.new(work_dir, user: "tester") }
+  subject { described_class.new("/", user: "tester") }
 
   describe "#config_file_path" do
-    it { expect(subject.config_file_path).to eq "/tmp/protocore/cluster_config.yml" }
+    it { expect(subject.config_file_path).to eq "/cluster_config.yml" }
   end
 
   describe "#root_path" do
-    it { expect(subject.root_path).to eq Pathname.new "/tmp/protocore" }
+    it { expect(subject.root_path).to eq Pathname.new "/" }
   end
 
   describe "#state_path" do
-    it { expect(subject.state_path).to eq Pathname.new "/tmp/protocore/.protocore" }
+    it { expect(subject.state_path).to eq Pathname.new "/.protocore" }
   end
 
   describe "#certs_path" do
-    it { expect(subject.certs_path).to eq Pathname.new "/tmp/protocore/.protocore/certs" }
+    it { expect(subject.certs_path).to eq Pathname.new "/.protocore/certs" }
   end
 
   describe "#keys_path" do
-    it { expect(subject.keys_path).to eq Pathname.new "/tmp/protocore/.protocore/keys" }
+    it { expect(subject.keys_path).to eq Pathname.new "/.protocore/keys" }
   end
 
   describe "#user" do
@@ -31,11 +30,11 @@ RSpec.describe Protocore::Context do
 
   describe "#manifest!" do
     it "it creates the all protocore state subdirectories" do
-      expect( Dir.exist? context.root_path ).to eq false
-      context.manifest!
-      expect( File.exist? context.config_file_path ).to eq true
-      expect( Dir.exist? context.keys_path ).to eq true
-      expect( Dir.exist? context.certs_path ).to eq true
+      expect( Dir.exist? subject.state_path ).to eq false
+      subject.manifest!
+      expect( File.exist? subject.config_file_path ).to eq true
+      expect( Dir.exist? subject.keys_path ).to eq true
+      expect( Dir.exist? subject.certs_path ).to eq true
     end
   end
 
