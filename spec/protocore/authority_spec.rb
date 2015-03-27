07e939018587825868fc7157d1e9ecfc1976177e
Philip Vieira <philip@vallin.se>
2015-03-27 03:01:19 +0000

    Remove artefacts from authority spec

--- spec/protocore/authority_spec.rb
+++ spec/protocore/authority_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Protocore::Authority do
 
-  include FakeFS::SpecHelpers
-
-  let!(:_context) { Protocore::Context.new("/").manifest! }
-
   let(:details) { { "common_name" => "1.test.local" } }
   let(:issuer) { { "country" => "GB", "city" => "London", "common_name" => "test.local" } }
 
@@ -22,8 +18,6 @@ RSpec.describe Protocore::Authority do
       expect(cert).to be_kind_of OpenSSL::X509::Certificate
       expect(cert.subject.to_s).to eq "/C=GB/L=London/CN=1.test.local"
       expect(cert.issuer.to_s).to eq "/C=GB/L=London/CN=test.local"
-      expect(File.exist?("/.protocore/certs/authorities/test.local.crt")).to eq false
-      expect(File.exist?("/.protocore/keys/authorities/test.local.pem")).to eq false
     end
   end
 
